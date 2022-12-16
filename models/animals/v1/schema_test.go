e13edea7b13e79be77a85c5af6c6aeac5bda729a
Paul Querna <pquerna@apache.org>
2022-12-15 21:49:33 -0800

    add schema migrations...

--- models/animals/v1/schema_test.go
+++ models/animals/v1/schema_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateSchema(t *testing.T) {
+func TestSchema(t *testing.T) {
 	ctx := context.Background()
 	pg, err := pgtest.Start()
 	require.NoError(t, err)
@@ -18,10 +18,32 @@ func TestCreateSchema(t *testing.T) {
 	_, err = pg.DB.Exec(ctx, "CREATE EXTENSION IF NOT EXISTS btree_gin")
 	require.NoError(t, err)
 
-	schema, err := pgdb_v1.CreateSchema(&Book{})
+	msg := &Pet{}
+	schema, err := pgdb_v1.CreateSchema(msg)
 	require.NoError(t, err)
 	for _, line := range schema {
 		_, err := pg.DB.Exec(ctx, line)
+		//		fmt.Printf("%s\n", line)
 		require.NoErrorf(t, err, "TestCreateSchema: failed to execute sql: '\n%s\n'", line)
 	}
+
+	// make sure we should have zero migrations after schema create
+	m, err := pgdb_v1.Migrations(ctx, pg.DB, msg)
+	require.NoError(t, err)
+	require.Len(t, m, 0)
+
+	// drop profile for fun
+	// it is both a col, and an index!
+	_, err = pg.DB.Exec(ctx, `ALTER TABLE pb_pet_models_animals_v1_8a3723d5 DROP COLUMN "pb$profile"`)
+	require.NoError(t, err)
+	m, err = pgdb_v1.Migrations(ctx, pg.DB, msg)
+	require.NoError(t, err)
+
+	require.Len(t, m, 2)
+	// fmt.Printf("-----\n%s\n", m[0])
+	require.Contains(t, m[0], "ALTER TABLE")
+	require.Contains(t, m[0], "pb$profile")
+	// fmt.Printf("-----\n%s\n", m[1])
+	require.Contains(t, m[1], "CREATE INDEX CONCURRENTLY IF NOT EXISTS")
+	require.Contains(t, m[1], "pb$profile")
 }
