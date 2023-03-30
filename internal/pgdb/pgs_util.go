84f0d66b393bf4906c1f3caf4a5adbdbf47fd078
Matt Kaniaris <matt.kaniaris@conductorone.com>
2023-03-30 20:31:28 +0000

    nethod -> method

--- internal/pgdb/pgs_util.go
+++ internal/pgdb/pgs_util.go
@@ -2,7 +2,6 @@ package pgdb
 
 import (
 	"fmt"
-	"os"
 
 	pgs "github.com/lyft/protoc-gen-star"
 )
@@ -18,25 +17,6 @@ func fieldByName(msg pgs.Message, name string) pgs.Field {
 func tryFieldByName(msg pgs.Message, name string) (pgs.Field, bool) {
 	for _, f := range msg.Fields() {
 		if f.Name().LowerSnakeCase().String() == name {
-			return f, true
-		}
-	}
-	return nil, false
-}
-
-func fieldByName2(fields []pgs.Field, name string) pgs.Field {
-	f, ok := tryFieldByName2(fields, name)
-	if !ok {
-		panic(fmt.Sprintf("Failed to find field %s", name))
-	}
-	return f
-}
-
-func tryFieldByName2(fields []pgs.Field, name string) (pgs.Field, bool) {
-	for _, f := range fields {
-		fName := f.Name().LowerSnakeCase().String()
-		fmt.Fprintf(os.Stderr, "🌮🌮🌮: %s\n", fName)
-		if fName == name {
 			return f, true
 		}
 	}
