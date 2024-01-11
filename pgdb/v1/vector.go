fd497ce86bfa8c7e1ed3631984fb36c09ea7fce2
Scott Mitchell <scott.mitchell@conductorone.com>
2024-01-11 18:43:00 +0000

    lint

--- pgdb/v1/vector.go
+++ pgdb/v1/vector.go
@@ -5,8 +5,7 @@ import (
 	"strings"
 )
 
-// In order to insert a vector type it needs to be of the form
-// '[1.0,2.0,3.0,...]'
+// In order to insert a vector type it needs to be of the form '[1.0,2.0,3.0,...]'.
 func FloatArrayToVectorString(in []float32) string {
 	return strings.Join(strings.Fields(fmt.Sprint(in)), ",")
 }
