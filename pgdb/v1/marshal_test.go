4251b6a7e82e8e74c9f9ca8c51b1e45461fae951
Owen Wardlaw <80442095+owardlaw@users.noreply.github.com>
2024-12-31 14:25:04 -0800

    [DUCT-6906] protojson.Marshal when proto -> JSON. (#87)
    * Use protojson.Marshal, want json bytes
    
    * Rm unused test
    
    * Lint

--- pgdb/v1/marshal_test.go
+++ pgdb/v1/marshal_test.go
@@ -1,8 +1,16 @@
 package v1
 
-import "testing"
+import (
+	reflect "reflect"
+	"strings"
+	"testing"
 
-// Test cases for CleanJSON
+	pstruct "github.com/golang/protobuf/ptypes/struct"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
 func TestCleanJSON(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -35,6 +43,11 @@ func TestCleanJSON(t *testing.T) {
 			input:    `{"key": "val\u0000ue\u0000"}`,
 			expected: `{"key":"value"}`,
 		},
+		{
+			name:     "Colon delimited string",
+			input:    `{"key": "111":222:3333:4444"}`,
+			expected: `{"key": "111":222:3333:4444"}`,
+		},
 	}
 
 	for _, test := range tests {
@@ -46,6 +59,142 @@ func TestCleanJSON(t *testing.T) {
 			}
 			if !test.hasError && string(result) != test.expected {
 				t.Errorf("Unexpected result. Got: %s, Expected: %s", result, test.expected)
+			}
+		})
+	}
+}
+
+func TestMarshalProtoJSON(t *testing.T) {
+	msg := &pstruct.Struct{
+		Fields: map[string]*pstruct.Value{
+			"PK:SK": {
+				Kind: &pstruct.Value_StringValue{
+					StringValue: "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl\u0000",
+				},
+			},
+			"SomeString": {
+				Kind: &pstruct.Value_StringValue{
+					StringValue: "\u0000uid_kdjkdjjdki:uid\u0000_ksdjfksdjfkjs:sdfl\u0000sdfjkl\u0000",
+				},
+			},
+		},
+	}
+
+	msg2 := &pstruct.Struct{
+		Fields: map[string]*pstruct.Value{
+			"PK:SK": {
+				Kind: &pstruct.Value_StringValue{
+					StringValue: "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl",
+				},
+			},
+			"SomeString": {
+				Kind: &pstruct.Value_StringValue{
+					StringValue: "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl",
+				},
+			},
+		},
+	}
+	msgExpected := &pstruct.Struct{
+		Fields: map[string]*pstruct.Value{
+			"PK:SK": {
+				Kind: &pstruct.Value_StringValue{
+					StringValue: "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl",
+				},
+			},
+			"SomeString": {
+				Kind: &pstruct.Value_StringValue{
+					StringValue: "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl",
+				},
+			},
+		},
+	}
+
+	msgExpectedBytes, err := protojson.Marshal(msgExpected)
+	if err != nil {
+		t.Errorf("Failed to marshal expected message: %v", err)
+	}
+
+	expected := string(msgExpectedBytes)
+	expectedNoSpace := strings.ReplaceAll(expected, " ", "")
+
+	tests := []struct {
+		name     string
+		input    *pstruct.Struct
+		expected string
+	}{
+		{
+			name:     "With null bytes",
+			input:    msg,
+			expected: expectedNoSpace,
+		},
+		{
+			name:     "Without null bytes",
+			input:    msg2,
+			expected: expected,
+		},
+	}
+
+	result, err := MarshalProtoJSON(msg)
+	if err != nil {
+		t.Fatalf("Expected an error when interpreting binary proto data as JSON, got none.\nResult: %s", string(result))
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := MarshalProtoJSON(test.input)
+			if err != nil {
+				t.Fatalf("Expected an error when interpreting binary proto data as JSON, got none.\nResult: %s", string(result))
+			}
+
+			if string(result) != test.expected {
+				t.Errorf("Unexpected result.\nGot:      %s\nExpected: %s", result, test.expected)
+			}
+		})
+	}
+}
+
+func TestMarshalJSON(t *testing.T) {
+	msg := map[string]interface{}{
+		"PK:SK":      "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl",
+		"SomeString": "\u0000String!!!! \u0000with\u0000",
+	}
+
+	msg2 := map[string]interface{}{
+		"PK:SK":      "uid_kdjkdjjdki:uid_ksdjfksdjfkjs:sdflsdfjkl",
+		"SomeString": "String!!!! with",
+	}
+
+	tests := []struct {
+		name     string
+		input    map[string]interface{}
+		expected map[string]interface{}
+	}{
+		{
+			name:     "With null bytes",
+			input:    msg,
+			expected: msg2,
+		},
+		{
+			name:     "Without null bytes",
+			input:    msg2,
+			expected: msg2,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := MarshalJSON(test.input)
+			if err != nil {
+				t.Fatalf("Expected an error when interpreting binary proto data as JSON, got none.\nResult: %s", string(result))
+			}
+
+			var got map[string]interface{}
+			if err := jsoniter.Unmarshal(result, &got); err != nil {
+				t.Fatalf("Failed to unmarshal JSON: %v", err)
+			}
+
+			if !reflect.DeepEqual(got, test.expected) {
+				t.Errorf("Unexpected result.\nGot:      %v\nExpected: %v", got, test.expected)
 			}
 		})
 	}
