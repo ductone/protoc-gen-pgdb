5832b9516ed93c99438b241a64cce5ebb89224a9
Logan Saso <logan.saso@conductorone.com>
2025-04-03 23:39:10 +0000

    Have the default be an empty string

--- internal/pgdb/pgdb_field.go
+++ internal/pgdb/pgdb_field.go
@@ -359,7 +359,7 @@ func getCommonFields(ctx pgsgo.Context, m pgs.Message, ix *importTracker) ([]*fi
 		DB: &pgdb_v1.Column{
 			Name:    "pkskv2",
 			Type:    vcDataType.Name,
-			Default: "'uninitialized'", // Will be replaced with actual value in codegen
+			Default: "''", // Will be replaced with actual value in codegen
 		},
 		GoName:   "PKSKV2",
 		DataType: vcDataType,
