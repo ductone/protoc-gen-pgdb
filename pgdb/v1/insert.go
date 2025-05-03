0376e0b58cb9ca6985b637af78511a4aec9176ca
Jessica McKinnie <jessica.mckinnie@conductorone.com>
2025-05-03 13:49:29 -0500

    demonstrate use of DescriptorWithPKSKV2 in comments

--- pgdb/v1/insert.go
+++ pgdb/v1/insert.go
@@ -21,16 +21,17 @@ func Insert(msg DBReflectMessage) (string, []any, error) {
 		return "", nil, errors.New("pgdb_v1: updated_at missing from message; unable to upsert without " + versionField.Name)
 	}
 
-	// postponing backfill until post migration
-	/* pkskv2Field := desc.PKSKV2Field()
-	if _, ok := record[pkskv2Field.Name]; !ok {
-		return "", nil, errors.New("pgdb_v1: pkskv2 missing from message; unable to upsert without " + pkskv2Field.Name)
-	}
+	/*
+		if descWithPkskv2, ok := desc.(DescriptorWithPKSKV2); ok {
+			pkskv2Field := descWithPkskv2.PKSKV2Field()
+			if _, ok := record[pkskv2Field.Name]; !ok {
+				return "", nil, errors.New("pgdb_v1: pkskv2 missing from message; unable to upsert without " + pkskv2Field.Name)
+			}
 
-	pk := record["pb$pk"]
-	sk := record["pb$sk"]
-	record[pkskv2Field.Name] = fmt.Sprintf("%s|%s", pk.(string), sk.(string))
-	*/
+			pk := record["pb$pk"]
+			sk := record["pb$sk"]
+			record[pkskv2Field.Name] = fmt.Sprintf("%s|%s", pk.(string), sk.(string))
+		} */
 
 	qb := goqu.Dialect("postgres")
 	q := qb.Insert(tableName).Prepared(true).Rows(
