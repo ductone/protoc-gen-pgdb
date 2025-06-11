fe9c42467855b1a22be98b0218400c74b140924b
chinkulkarni <chinmay.kulkarni@conductorone.com>
2025-06-11 17:20:15 +0000

    fix bit vector sizing

--- pgdb/v1/xpq/bits_value.go
+++ pgdb/v1/xpq/bits_value.go
@@ -11,7 +11,7 @@ import (
 type BitsValue []byte
 
 func (b BitsValue) Value() (driver.Value, error) {
-	bytesLen, err := safeIntToInt32(len(b))
+	bitsLen, err := safeIntToInt32(len(b) * 8)
 	if err != nil {
 		return nil, err
 	}
@@ -19,7 +19,7 @@ func (b BitsValue) Value() (driver.Value, error) {
 	bits := &pgtype.Bits{
 		Bytes: b,
 		Valid: true,
-		Len:   bytesLen,
+		Len:   bitsLen,
 	}
 
 	return bits.Value()
